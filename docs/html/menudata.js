f7a90c465ea1546643fa217a0eb1121bab1ac7b9
Lin,Chi <lin.chi@hzleaper.com>
2021-11-23 12:59:25 +0800

    add content.

---
+++ docs/html/menudata.js
@@ -0,0 +1,37 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"首页",url:"index.html"},
+{text:"相关页面",url:"pages.html"},
+{text:"基础功能",url:"namespace_l_p_v_core_lib.html"},
+{text:"几何",url:"namespace_l_p_v_geom_lib.html"},
+{text:"图像处理",url:"namespace_l_p_v_img_proc_lib.html"},
+{text:"目标定位",url:"namespace_l_p_v_locate_lib.html"},
+{text:"模板匹配",url:"namespace_l_p_v_pat_lib.html"},
+{text:"斑点分析",url:"namespace_l_p_v_blob_lib.html"},
+{text:"相机标定",url:"namespace_l_p_v_calib_lib.html"},
+{text:"条码检测",url:"namespace_l_p_v_barcode_lib.html"},
+{text:"连接到灵闪 IntelliBlink",url:"namespace_l_p_v_i_b_lib.html"},
+{text:"显示控件",url:"namespace_l_p_v_display_lib.html"}]}
