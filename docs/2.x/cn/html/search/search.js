9851e33faa2cacc1f71ca1de3b94a7b827c8105e
Lin,Chi <lin.chi@hzleaper.com>
2024-12-23 14:00:00 +0800

    update search js.

--- docs/2.x/cn/html/search/search.js
+++ docs/2.x/cn/html/search/search.js
@@ -356,7 +356,7 @@ function SearchBox(name, resultsPath, inFrame, label, extension)
     else // nothing available for this search term
     {
        resultsPage = this.resultsPath + '/nomatches' + this.extension;
-       resultsPageWithSearch = resultsPage;
+       resultsPageWithSearch = resultsPage+'?'+searchValue;
        hasResultsPage = false;
     }
 
